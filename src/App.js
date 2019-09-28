341a6c4fe39457500e09a054befb749ed1566c8a
Shafran <Shafran@ieee.org>
2019-09-28 16:07:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-import { MDBBtn, MDBFormInline, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { MDBBtn, MDBCard, MDBView, MDBCardImage, MDBCardTitle, MDBCardText, MDBCardBody, MDBFormInline, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import Img from 'react-image'
 import './App.css';
+import axios from 'axios'
+
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+
+    }
+  }
+
+  componentDidMount() {
+    fetch('./coredata.json').then(response => {
+      console.log(response);
+      return response.json();
+    }).then(data => {
+      // Work with JSON data here
+      console.log(data.movies);
+      this.setState({
+        movies: data.movies
+      })
+    }).catch(err => {
+      // Do something for an error here
+      console.log("Error Reading data " + err);
+    });
+  }
+
   render() {
     return (
       <MDBContainer>
         <MDBRow>
-        <MDBCol>
+          <MDBCol>
             <img className='s' src='https://i.imgur.com/WTGKUij.png'></img>
           </MDBCol>
-           <MDBCol>
+          <MDBCol>
             <img src='https://i.imgur.com/l8vyr9X.png'></img>
           </MDBCol>
           <MDBCol></MDBCol>
@@ -28,10 +55,47 @@ class App extends Component {
           </MDBCol>
 
         </MDBRow>
+        {/* Card */}
+
+
+        <MDBRow>
+
+          <div>
+            {
+              this.state.movies.map((value) =>
+                <div className="fetch">
+
+                  <span>
+                    <MDBCol style={{ maxWidth: "20rem" }}>
+
+                      <MDBCard>
+
+                        <MDBCardImage className="img-fluid" src="https://i.imgur.com/D0jm0sc.jpg" />
+                        <MDBCardBody>
+                          <MDBIcon icon='film' /> Movie
+    <MDBCardTitle className='font-weight-bold'><strong>{value.title}</strong> <MDBBadge color="primary" pill>1997</MDBBadge></MDBCardTitle>
+                          <MDBBadge pill color="warning">Drama</MDBBadge> <MDBBadge pill color="warning">Romance</MDBBadge> <br></br>
+
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBCol>
+
+
+
+                  </span>
+                </div>
+              )
+            }
+          </div>
+
+
+        </MDBRow>
 
       </MDBContainer>
     );
   }
 }
+
+
 
 export default App;
